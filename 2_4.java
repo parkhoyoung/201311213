45bd96f5fe87d82a1e97e8a643c25ef076ca324b
parkhoyoung <hopkm1608@naver.com>
2016-09-21 14:45:28 +0900

    Add files via upload

---
+++ 2_4.java
@@ -0,0 +1,11 @@
+class Person{
+private float weight;
+public void setWeight(float w){
+this.weight=w;
+}
+private float getWeight(){
+return weight;
+}
+}
+Person me = new Person();
+me.setWeight((float)60.0);
